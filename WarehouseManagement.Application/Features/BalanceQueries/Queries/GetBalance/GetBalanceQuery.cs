ffd664a9317e0927faf79f9016b9931de3374490
vjmurenko <vjmurenko@gmail.com>
2025-08-26 15:23:27 +0300

    Delete files

--- WarehouseManagement.Application/Features/BalanceQueries/Queries/GetBalance/GetBalanceQuery.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using WarehouseManagement.Application.Features.BalanceQueries.DTOs;
-
-namespace WarehouseManagement.Application.Features.BalanceQueries.Queries.GetBalance;
-
-public record GetBalanceQuery(
-    List<Guid>? ResourceIds = null,
-    List<Guid>? UnitOfMeasureIds = null
-) : IRequest<List<BalanceDto>>;
--- WarehouseManagement.Application/Features/ShipmentDocuments/Commands/SignShipment/SignShipmentCommand.cs
+++
@@ -1,5 +0,0 @@
-using MediatR;
-
-namespace WarehouseManagement.Application.Features.ShipmentDocuments.Commands.SignShipment;
-
-public record SignShipmentCommand(Guid ShipmentDocumentId) : IRequest<bool>;
