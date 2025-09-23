b6256b014d980af3d87348f539ec5d73b4232a0b
vjmurenko <vjmurenko@gmail.com>
2025-09-23 12:50:49 +0300

    Change data local to RU

--- WarehouseManagement.UI/src/pages/ReceiptDocuments/ReceiptsPage.tsx
+++ WarehouseManagement.UI/src/pages/ReceiptDocuments/ReceiptsPage.tsx
@@ -53,7 +53,7 @@ const ReceiptsPage: React.FC = () => {
 
   const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    return date.toLocaleDateString('en-US', {
+    return date.toLocaleDateString('ru-RU', {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric'
